17e4798dbd250c40b6abcb15cb6e4eae1455c1ab
Hans Bala <hansbala@hansbala.com>
2020-03-02 23:35:26 -0500

    done

---
+++ components/CreateTweet.js
@@ -0,0 +1,10 @@
+export default {
+    template: `
+    <div class="new-tweet-area">
+        <textarea id="write-tweet-area" rows="7" cols="40" placeholder="Write a new tweet here..."></textarea>
+        <div class="submit-btn-area">
+            <button id="submit-btn"> Tweet </button>
+        </div>
+    </div>
+    `
+}
